7047053e0a3e1e710e1fc2bdb632e338dbfe6753
acao <alc3pf@virginia.edu>
2020-05-26 13:07:35 -0400

    add build

---
+++ build/precache-manifest.5b326de163b18be1da63af513bfa093f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3f1f0278343c21ad3b7432d804c37b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1e84f2e84de43b658e2",
+    "url": "/static/css/main.ff262557.chunk.css"
+  },
+  {
+    "revision": "4abf5feb189faf887d48",
+    "url": "/static/js/2.6ede50da.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.6ede50da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e84f2e84de43b658e2",
+    "url": "/static/js/main.0e5021b6.chunk.js"
+  },
+  {
+    "revision": "88f5be2688e5972e0368",
+    "url": "/static/js/runtime-main.2867fe69.js"
+  }
+]);
